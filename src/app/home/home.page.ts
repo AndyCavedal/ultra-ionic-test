185a8c4f32097b5d4a847f9246d8d871266bc2e7
Andy <hcavedal@gmail.com>
2023-10-17 18:02:19 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  headerImg: string = 'https://tarjetaultra.com/wp-content/uploads/2022/09/Copia-de-logo-ULTRA-1.png';
+
+  slideImgs: string[] = [
+    'https://www.ultr.ar/admin/assets/img/slide1.png',
+    'https://www.ultr.ar/admin/assets/img/slide2.png',
+    'https://www.ultr.ar/admin/assets/img/slide3.png'
+  ];
+
+  slideOpts = {
+    initialSlide: 0, // Índice de la diapositiva inicial
+    speed: 400, // Velocidad de transición en milisegundos
+    loop: true, // Bucle infinito
+    centeredSlides: true, // Centra la diapositiva activa
+    slidesPerView: 1, // Número de diapositivas visibles
+    spaceBetween: 10, // Espacio entre las diapositivas
+  };
+
   constructor() {}
 
 }
